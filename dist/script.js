eea86ddf2c0a9cbc5f088034a9f6f154bc040c9a
DennisFlink <dennis.sabriev@outlook.com>
2024-01-23 10:58:34 +0100

    Dennis provar saker

--- dist/script.js
+++ dist/script.js
@@ -7,79 +7,85 @@ const easytimer_js_1 = __importDefault(require("easytimer.js"));
 let timer = new easytimer_js_1.default();
 let visualScreen = false;
 let interval = true;
-const header = document.querySelector('header');
-const setTimer = document.querySelector('.set-timer');
-const visual = document.querySelector('.visual');
-const analogTimer = document.querySelector('.analog-timer');
-const pauseView = document.querySelector('.pause-view');
-const digitalTimer = document.querySelector('.digital-timer');
-const alarm = document.querySelector('.alarm');
-const logoBtn = document.getElementById('logo');
-const startBtn = document.getElementById('start');
-const abortBtns = document.querySelectorAll('.abort-timer-button');
-logoBtn.addEventListener('click', function () {
+const header = document.querySelector("header");
+const setTimer = document.querySelector(".set-timer");
+const visual = document.querySelector(".visual");
+const analogTimer = document.querySelector(".analog-timer");
+const pauseView = document.querySelector(".pause-view");
+const digitalTimer = document.querySelector(".digital-timer");
+const alarm = document.querySelector(".alarm");
+const logoBtn = document.getElementById("logo");
+const startBtn = document.getElementById("startTimer");
+const abortBtns = document.querySelectorAll(".abort-timer-button");
+const counterValue = document.getElementById("minutes");
+const conutUpBtn = document.getElementById("countUp");
+const countDownBtn = document.getElementById("countDown");
+logoBtn.addEventListener("click", function () {
     goToSetTimer();
 });
-abortBtns.forEach(button => {
-    button.addEventListener('click', function () {
+abortBtns.forEach((button) => {
+    button.addEventListener("click", function () {
         goToSetTimer();
     });
 });
-startBtn.addEventListener('click', function () {
+startBtn.addEventListener("click", function () {
     startTimer();
 });
 const goToSetTimer = () => {
-    const sectionsToHide = document.querySelectorAll('section:not(#header):not(#setTimer)');
-    sectionsToHide.forEach(section => section.classList.add('hidden'));
-    header.classList.remove('hidden');
-    setTimer.classList.remove('hidden');
+    const sectionsToHide = document.querySelectorAll("section:not(#header):not(#setTimer)");
+    sectionsToHide.forEach((section) => section.classList.add("hidden"));
+    header.classList.remove("hidden");
+    setTimer.classList.remove("hidden");
 };
 const goToVisual = () => {
-    const sectionsToHide = document.querySelectorAll('section:not(#header):not(#visual)');
-    sectionsToHide.forEach(section => section.classList.add('hidden'));
-    header.classList.remove('hidden');
-    visual.classList.remove('hidden');
+    const sectionsToHide = document.querySelectorAll("section:not(#header):not(#visual)");
+    sectionsToHide.forEach((section) => section.classList.add("hidden"));
+    header.classList.remove("hidden");
+    visual.classList.remove("hidden");
     visualScreen = true;
 };
 const goToAnalogTimer = () => {
-    const sectionsToHide = document.querySelectorAll('section:not(#header):not(#analogTimer)');
-    sectionsToHide.forEach(section => section.classList.add('hidden'));
-    header.classList.remove('hidden');
-    analogTimer.classList.remove('hidden');
+    const sectionsToHide = document.querySelectorAll("section:not(#header):not(#analogTimer)");
+    sectionsToHide.forEach((section) => section.classList.add("hidden"));
+    header.classList.remove("hidden");
+    analogTimer.classList.remove("hidden");
     visualScreen = false;
 };
 const goToDigitalTimer = () => {
-    const sectionsToHide = document.querySelectorAll('section:not(#header):not(#ditialTimer)');
-    sectionsToHide.forEach(section => section.classList.add('hidden'));
-    header.classList.remove('hidden');
-    digitalTimer.classList.remove('hidden');
+    const sectionsToHide = document.querySelectorAll("section:not(#header):not(#ditialTimer)");
+    sectionsToHide.forEach((section) => section.classList.add("hidden"));
+    header.classList.remove("hidden");
+    digitalTimer.classList.remove("hidden");
     visualScreen = false;
 };
 const goToPauseView = () => {
-    const sectionsToHide = document.querySelectorAll('section:not(#pauseView)');
-    sectionsToHide.forEach(section => section.classList.add('hidden'));
-    pauseView.classList.remove('hidden');
-    header.classList.add('hidden');
+    const sectionsToHide = document.querySelectorAll("section:not(#pauseView)");
+    sectionsToHide.forEach((section) => section.classList.add("hidden"));
+    pauseView.classList.remove("hidden");
+    header.classList.add("hidden");
     visualScreen = false;
     startIntervalTimer();
 };
 const startIntervalTimer = () => {
-    const initialTime = { minutes: 5 };
+    const initialTime = { seconds: 5 };
     timer.start({ countdown: true, startValues: initialTime });
+    console.log("start interval timer");
     let minute = document.getElementById("interval-minutes");
     let second = document.getElementById("interval-seconds");
-    minute.textContent = timer.getTimeValues().minutes.toString();
-    second.textContent = timer.getTimeValues().seconds.toString();
+    timer.addEventListener("secondsUpdated", function () {
+        minute.textContent = timer.getTimeValues().minutes.toString();
+        second.textContent = timer.getTimeValues().seconds.toString();
+    });
 };
 const goToAlarm = () => {
     if (interval === true) {
         goToPauseView();
     }
     else {
-        const sectionsToHide = document.querySelectorAll('section:not(#alarm)');
-        sectionsToHide.forEach(section => section.classList.add('hidden'));
-        header.classList.add('hidden');
-        alarm.classList.remove('hidden');
+        const sectionsToHide = document.querySelectorAll("section:not(#alarm)");
+        sectionsToHide.forEach((section) => section.classList.add("hidden"));
+        header.classList.add("hidden");
+        alarm.classList.remove("hidden");
         resetClock();
         visualScreen = false;
     }
@@ -91,25 +97,23 @@ if (amountMinutesElement) {
     numericValue = parseInt(amountMinutes, 10);
 }
 const initialTime = { seconds: numericValue };
-timer.start({ countdown: true, startValues: initialTime });
-console.log('timer sttarted');
-timer.addEventListener("secondsUpdated", function (e) {
-    let basicUsageElement = document.querySelector("#gettingvalue");
-    if (basicUsageElement) {
+console.log(initialTime);
+const startTimer = () => {
+    timer.start({ countdown: true, startValues: initialTime });
+    console.log("timer started");
+    timer.addEventListener("secondsUpdated", function () {
         let minute = document.querySelector(".minutes");
         let second = document.querySelector(".seconds");
         minute.textContent = timer.getTimeValues().minutes.toString();
         second.textContent = timer.getTimeValues().seconds.toString();
-    }
-    visualTimer();
-    startClock();
-});
-const startTimer = () => {
+        visualTimer();
+        startClock();
+    });
+    timer.addEventListener("targetAchieved", function (e) {
+        goToAlarm();
+        console.log("timer ended!");
+    });
 };
-timer.addEventListener('targetAchieved', function (e) {
-    goToAlarm();
-    console.log('timer ended!');
-});
 const visualTimer = () => {
     let totalTimeInSeconds = timer.getTotalTimeValues().seconds;
     upDateProg(totalTimeInSeconds);
@@ -157,3 +161,14 @@ function resetClock() {
 function rotateClockHand(element, rotation) {
     element.style.setProperty("--rotate", `${rotation * 360}`);
 }
+let minutes = 1;
+conutUpBtn.addEventListener("click", () => {
+    minutes++;
+    counterValue.innerHTML = minutes.toString();
+});
+countDownBtn.addEventListener("click", () => {
+    if (minutes > 1) {
+        minutes--;
+        counterValue.innerHTML = minutes.toString();
+    }
+});
--- script.ts
+++ script.ts
@@ -4,96 +4,106 @@ let timer = new Timer();
 let visualScreen: boolean = false;
 let interval: boolean = true;
 
-const header = document.querySelector('header') as HTMLElement
-const setTimer = document.querySelector('.set-timer') as HTMLElement
-const visual = document.querySelector('.visual') as HTMLElement
-const analogTimer = document.querySelector('.analog-timer') as HTMLElement
-const pauseView = document.querySelector('.pause-view') as HTMLElement
-const digitalTimer = document.querySelector('.digital-timer') as HTMLElement
-const alarm = document.querySelector('.alarm') as HTMLElement
-const logoBtn = document.getElementById('logo') as HTMLElement
-const startBtn = document.getElementById('start') as HTMLElement
-const abortBtns: NodeListOf<HTMLButtonElement> = document.querySelectorAll('.abort-timer-button')
+const header = document.querySelector("header") as HTMLElement;
+const setTimer = document.querySelector(".set-timer") as HTMLElement;
+const visual = document.querySelector(".visual") as HTMLElement;
+const analogTimer = document.querySelector(".analog-timer") as HTMLElement;
+const pauseView = document.querySelector(".pause-view") as HTMLElement;
+const digitalTimer = document.querySelector(".digital-timer") as HTMLElement;
+const alarm = document.querySelector(".alarm") as HTMLElement;
+const logoBtn = document.getElementById("logo") as HTMLElement;
+const startBtn = document.getElementById("startTimer") as HTMLElement;
+const abortBtns: NodeListOf<HTMLButtonElement> = document.querySelectorAll(
+  ".abort-timer-button"
+);
+const counterValue = document.getElementById("minutes") as HTMLElement;
+const conutUpBtn = document.getElementById("countUp") as HTMLElement;
+const countDownBtn = document.getElementById("countDown") as HTMLElement;
 
-logoBtn.addEventListener('click', function(){
-  goToSetTimer()
-})
-
-abortBtns.forEach(button => {
-  button.addEventListener('click', function() {
-    goToSetTimer()
-  })
+logoBtn.addEventListener("click", function () {
+  goToSetTimer();
 });
 
-startBtn.addEventListener('click', function() {
-  startTimer()
-})
+abortBtns.forEach((button) => {
+  button.addEventListener("click", function () {
+    goToSetTimer();
+  });
+});
+
+startBtn.addEventListener("click", function () {
+  startTimer();
+});
 
 const goToSetTimer = () => {
-  const sectionsToHide = document.querySelectorAll('section:not(#header):not(#setTimer)');
-  sectionsToHide.forEach(section => section.classList.add('hidden'));
-  header.classList.remove('hidden');
-  setTimer.classList.remove('hidden');
+  const sectionsToHide = document.querySelectorAll(
+    "section:not(#header):not(#setTimer)"
+  );
+  sectionsToHide.forEach((section) => section.classList.add("hidden"));
+  header.classList.remove("hidden");
+  setTimer.classList.remove("hidden");
 };
 const goToVisual = () => {
-  const sectionsToHide = document.querySelectorAll('section:not(#header):not(#visual)');
-  sectionsToHide.forEach(section => section.classList.add('hidden'));
-  header.classList.remove('hidden');
-  visual.classList.remove('hidden');
+  const sectionsToHide = document.querySelectorAll(
+    "section:not(#header):not(#visual)"
+  );
+  sectionsToHide.forEach((section) => section.classList.add("hidden"));
+  header.classList.remove("hidden");
+  visual.classList.remove("hidden");
   visualScreen = true;
 };
 const goToAnalogTimer = () => {
-  const sectionsToHide = document.querySelectorAll('section:not(#header):not(#analogTimer)');
-  sectionsToHide.forEach(section => section.classList.add('hidden'));
-  header.classList.remove('hidden');
-  analogTimer.classList.remove('hidden');
+  const sectionsToHide = document.querySelectorAll(
+    "section:not(#header):not(#analogTimer)"
+  );
+  sectionsToHide.forEach((section) => section.classList.add("hidden"));
+  header.classList.remove("hidden");
+  analogTimer.classList.remove("hidden");
   visualScreen = false;
 };
 const goToDigitalTimer = () => {
-  const sectionsToHide = document.querySelectorAll('section:not(#header):not(#ditialTimer)');
-  sectionsToHide.forEach(section => section.classList.add('hidden'));
-  header.classList.remove('hidden');
-  digitalTimer.classList.remove('hidden');
+  const sectionsToHide = document.querySelectorAll(
+    "section:not(#header):not(#ditialTimer)"
+  );
+  sectionsToHide.forEach((section) => section.classList.add("hidden"));
+  header.classList.remove("hidden");
+  digitalTimer.classList.remove("hidden");
   visualScreen = false;
 };
 const goToPauseView = () => {
-  const sectionsToHide = document.querySelectorAll('section:not(#pauseView)');
-  sectionsToHide.forEach(section => section.classList.add('hidden'));
-  pauseView.classList.remove('hidden');
-  header.classList.add('hidden')
+  const sectionsToHide = document.querySelectorAll("section:not(#pauseView)");
+  sectionsToHide.forEach((section) => section.classList.add("hidden"));
+  pauseView.classList.remove("hidden");
+  header.classList.add("hidden");
   visualScreen = false;
 
-  startIntervalTimer()
+  startIntervalTimer();
 };
 
 const startIntervalTimer = () => {
-  const initialTime = { minutes: 5 };
+  const initialTime = { seconds: 5 };
   timer.start({ countdown: true, startValues: initialTime });
+  console.log("start interval timer");
+  let minute = document.getElementById("interval-minutes") as HTMLElement;
+  let second = document.getElementById("interval-seconds") as HTMLElement;
 
-  let minute = document.getElementById("interval-minutes");
-  let second = document.getElementById("interval-seconds");
-
-  minute!.textContent = timer.getTimeValues().minutes.toString();
-  second!.textContent = timer.getTimeValues().seconds.toString();
-
-  
-}
+  timer.addEventListener("secondsUpdated", function () {
+    minute!.textContent = timer.getTimeValues().minutes.toString();
+    second!.textContent = timer.getTimeValues().seconds.toString();
+  });
+};
 
 const goToAlarm = () => {
   if (interval === true) {
-    goToPauseView()
-  }
-  else {
-    const sectionsToHide = document.querySelectorAll('section:not(#alarm)');
-    sectionsToHide.forEach(section => section.classList.add('hidden'));
-    header.classList.add('hidden')
-    alarm.classList.remove('hidden');
-    resetClock()
+    goToPauseView();
+  } else {
+    const sectionsToHide = document.querySelectorAll("section:not(#alarm)");
+    sectionsToHide.forEach((section) => section.classList.add("hidden"));
+    header.classList.add("hidden");
+    alarm.classList.remove("hidden");
+    resetClock();
     visualScreen = false;
   }
-}
-
-
+};
 
 let numericValue: number = 0;
 const amountMinutesElement = document.getElementById("minutes");
@@ -104,28 +114,26 @@ if (amountMinutesElement) {
 }
 
 const initialTime = { seconds: numericValue };
-
-timer.start({ countdown: true, startValues: initialTime });
-console.log('timer sttarted')
-timer.addEventListener("secondsUpdated", function (e) {
-  let basicUsageElement = document.querySelector("#gettingvalue");
-  if (basicUsageElement) {
+console.log(initialTime);
+const startTimer = () => {
+  timer.start({ countdown: true, startValues: initialTime });
+  console.log("timer started");
+  timer.addEventListener("secondsUpdated", function () {
     let minute = document.querySelector(".minutes");
     let second = document.querySelector(".seconds");
 
     minute!.textContent = timer.getTimeValues().minutes.toString();
     second!.textContent = timer.getTimeValues().seconds.toString();
-  }
-  visualTimer();
-  startClock();
-});
-const startTimer = () => {
-}
 
-timer.addEventListener('targetAchieved', function(e) {
-  goToAlarm()
-  console.log('timer ended!')
-})
+    visualTimer();
+    startClock();
+  });
+
+  timer.addEventListener("targetAchieved", function (e) {
+    goToAlarm();
+    console.log("timer ended!");
+  });
+};
 
 const visualTimer = () => {
   let totalTimeInSeconds = timer.getTotalTimeValues().seconds;
@@ -139,7 +147,6 @@ const upDateProg = (totalTimeInSeconds: any) => {
   const navH1: HTMLElement | null = navContainer!.querySelector(".nav > h1");
   const menuIcon: HTMLElement | null = document.querySelector(".menu-icon");
 
-
   if (progressBarEl && navContainer && navH1 && menuIcon) {
     const containerHeight: number = navContainer.offsetHeight;
     let progress: number =
@@ -149,7 +156,7 @@ const upDateProg = (totalTimeInSeconds: any) => {
 
     progressBarEl.style.height = `${progress}%`;
 
-     if (visualScreen === true && progress >= containerHeight - 27) {
+    if (visualScreen === true && progress >= containerHeight - 27) {
       navH1.classList.add("white-text");
       menuIcon.classList.add("white-text");
     } else {
@@ -170,12 +177,12 @@ const hourHand = document.getElementById("hour-hand") as HTMLElement;
 let elapsedSeconds: number = 0;
 
 function startClock() {
-    elapsedSeconds++;
-    const seconds = elapsedSeconds / 60;
-    const minutes = seconds / 60;
+  elapsedSeconds++;
+  const seconds = elapsedSeconds / 60;
+  const minutes = seconds / 60;
 
-    rotateClockHand(secondHand, seconds);
-    rotateClockHand(minutedHand, minutes);
+  rotateClockHand(secondHand, seconds);
+  rotateClockHand(minutedHand, minutes);
 }
 
 function resetClock() {
@@ -187,3 +194,17 @@ function resetClock() {
 function rotateClockHand(element: HTMLElement, rotation: number) {
   element.style.setProperty("--rotate", `${rotation * 360}`);
 }
+
+let minutes: number = 1;
+
+conutUpBtn.addEventListener("click", () => {
+  minutes++;
+  counterValue.innerHTML = minutes.toString();
+});
+
+countDownBtn.addEventListener("click", () => {
+  if (minutes > 1) {
+    minutes--;
+    counterValue.innerHTML = minutes.toString();
+  }
+});
